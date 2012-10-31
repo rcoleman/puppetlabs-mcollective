b8fcf52900447c8ba347aef0c44082327d737e23
Ryan Coleman <ryan@puppetlabs.com>
2012-10-31 15:45:31 -0700

    Collapse client classes into client.pp
    Prior to this commit, all the MCollective client configuration
    occurred within several client:: subclasses. This is undesirable
    from a management and best practices stand-point.
    
    This commit moves that configuration into a mcollective::client
    class and modifies the mcollective class to use the new class. No
    other behavior was modified.

---
+++ manifests/client.pp
@@ -0,0 +1,44 @@
+# Class: mcollective::client
+#
+#   This class installs the MCollective client component for your nodes.
+#
+# Parameters:
+#
+#  [*version*]            - The version of the MCollective package(s) to
+#                             be installed.
+#  [*config*]             - The content of the MCollective client configuration
+#                             file.
+#  [*config_file*]        - The full path to the MCollective client
+#                             configuration file.
+#  [*manage_packages*]    - Whether or not to manage the client packages. Accepts
+#                              ensure or absent.
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class mcollective::client(
+  $version,
+  $config,
+  $manage_packages,
+  $config_file
+) inherits mcollective::params { 
+
+  if $manage_packages {
+	 	package { 'mcollective-client':
+	    ensure => $version,
+			before => File['client_config'],
+	  }
+  }
+		
+	file { 'client_config':
+	  ensure  => present,
+    path    => $config_file,
+    content => $config,
+    mode    => '0600',
+    owner   => $client_config_owner,
+    group   => $client_config_group,
+  }
+
+}
