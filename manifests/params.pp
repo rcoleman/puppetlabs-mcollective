7721328f7d5d8726e3521ea142bb5400118b83fc
Ryan Coleman <ryan@puppetlabs.com>
2012-11-01 13:02:38 -0700

    Convert params.pp to paramertized class.
    Prior to this commit, the mcollective::params class set a bunch of
    parameter defaults but did not do so as a parameterized class. This
    limits the user interface for overriding these defaults to accomodate
    for different scnearios.
    
    This commit converts the class into a parameterized class and alters
    some values in the client template to use the fully qualified variable
    lookup.

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,44 +11,56 @@
 #
 # Sample Usage:
 #
-class mcollective::params {
-  $version              = 'UNSET'
-  $enterprise           = false
-  $manage_packages      = true
-  $manage_plugins       = false
-  $server               = true
-  $server_config        = 'UNSET'
-  $server_config_file   = '/etc/mcollective/server.cfg'
-  $client               = false
-  $client_config        = 'UNSET'
-  $client_config_file   = '/etc/mcollective/client.cfg'
-  $main_collective      = 'mcollective'
-  $collectives          = 'mcollective'
-  $connector            = 'stomp'
-  $classesfile          = '/var/lib/puppet/state/classes.txt'
-  $stomp_pool           = 'UNSET'
-  $mc_topicprefix       = '/topic/'
-  $mc_main_collective   = 'mcollective'
-  $mc_collectives       = ''
-  $mc_logfile           = '/var/log/mcollective.log'
-  $mc_loglevel          = 'log'
-  $mc_daemonize         = '1'
-  $mc_security_provider = 'psk'
-  $mc_security_psk      = 'changemeplease'
-  $fact_source          = 'facter'
-  $yaml_facter_source   = '/etc/mcollective/facts.yaml'
-  $plugin_params        = {}
+class mcollective::params (
+	$version              = 'UNSET',
+	$enterprise           = false,
+	$manage_packages      = true,
+	$manage_plugins       = false,
+	$server               = true,
+	$server_config        = 'UNSET',
+	$server_config_file   = '/etc/mcollective/server.cfg',
+	$client               = false,
+	$client_config        = 'UNSET',
+	$client_config_file   = '/etc/mcollective/client.cfg',
+	$main_collective      = 'mcollective',
+	$collectives          = 'mcollective',
+	$connector            = 'stomp',
+	$classesfile          = '/var/lib/puppet/state/classes.txt',
+	$stomp_pool           = 'UNSET',
+	$mc_topicprefix       = '/topic/',
+	$mc_main_collective   = 'mcollective',
+	$mc_collectives       = '',
+	$mc_logfile           = '/var/log/mcollective.log',
+	$mc_loglevel          = 'log',
+	$mc_daemonize         = '1',
+	$mc_security_provider = 'psk',
+	$mc_security_psk      = 'changemeplease',
+	$fact_source          = 'facter',
+	$yaml_facter_source   = '/etc/mcollective/facts.yaml',
+	$plugin_params        = {},
+	$stomp_user    = 'mcollective',
+  $stomp_passwd  = 'marionette',
+  $stomp_server  = 'stomp',
+  $stomp_port    = '6163',
+  $client_config_owner  = '0',
+  $client_config_group  = '0',
+  $server_config_owner  = '0',
+  $server_config_group  = '0',
+  $pkg_state = 'present'
+) {
+
 
   case $osfamily {
     'redhat': {
-      $nrpe_dir_real = '/etc/nrpe.d'
-      $mc_service_start = '/sbin/service mcollective start',
-      $mc_service_stop  = '/sbin/service mcollective stop',
+      $nrpe_dir_real    = '/etc/nrpe.d'
+      $mc_service_start = '/sbin/service mcollective start'
+      $mc_service_stop  = '/sbin/service mcollective stop'
+      $mc_libdir        = '/usr/libexec/mcollective'
     }
     'debian': {
-      $mc_libdir = '/usr/share/mcollective/plugins'
-      $mc_service_start = '/etc/init.d/mcollective start', 
-      $mc_service_stop  = '/etc/init.d/mcollective stop',
+      $mc_libdir        = '/usr/share/mcollective/plugins'
+      $mc_service_start = '/etc/init.d/mcollective start' 
+      $mc_service_stop  = '/etc/init.d/mcollective stop'
     }
     'darwin': {
       $mc_service_name = 'com.puppetlabs.mcollective'
@@ -73,16 +85,5 @@ class mcollective::params {
     "${plugin_base}/util",
   ]
 
-  $client_config_owner  = '0'
-  $client_config_group  = '0'
-  $server_config_owner  = '0'
-  $server_config_group  = '0'
-
-  $stomp_user    = 'mcollective'
-  $stomp_passwd  = 'marionette'
-  $stomp_server  = 'stomp'
-  $stomp_port    = '6163'
-
-  $pkg_state = 'present'
 
 }
