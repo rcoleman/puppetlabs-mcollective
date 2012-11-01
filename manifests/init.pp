deb5ca42b2209b6d521edffccf1d2c01867fd1b5
Gary Larizza <gary@puppetlabs.com>
2012-11-01 12:44:26 -0700

    Simplify mcollective class
    Previously, the mcollective class accepted a large number of parameters
    that would be evaluated and used to configure all subcomponents of
    MCollective.  This made the mcollective class unwieldy, but also led to
    some confusion once enterprise support was added. To ease the process
    in getting MCollective setup, it was decided to expose all functional
    parameters to the main mcollective class (i.e. Should MCollective be
    setup with Puppet Enterprise? Should the MCollective client component
    be installed? Should this class manage the installation of MCollective
    plugins? etc...) and let the mcollective::params be the entry point for
    more granular configuration changes.
    
    This commit updates the main mcollective class so that it only takes five
    functional parameters: enterprise, client, server, version, and
    manage_plugins.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # Class: mcollective
 #
-# This module manages MCollective.
+# This module manages MCollective. All custom parameter values can be
+# changed by modifying the mcollective::params class.
 #
 # Parameters:
 #
@@ -8,45 +9,14 @@
 #                             be installed.
 #  [*server*]             - Boolean determining whether you would like to
 #                             install the server component.
-#  [*manage_packages]     - Boolean determining whether module should install
-#                           required packages
 #  [*manage_plugins]      - Boolean controlling installation of plugins in module
-#  [*server_config*]      - The content of the MCollective server configuration
-#                             file.
-#  [*server_config_file*] - The full path to the MCollective server
 #                             configuration file.
 #  [*client*]             - Boolean determining whether you would like to
 #                             install the client component.
-#  [*client_config*]      - The content of the MCollective client configuration
-#                             file.
-#  [*client_config_file*] - The full path to the MCollective client
-#                             configuration file.
-#  [*stomp_server*]       - The hostname of the stomp server.
-#  [*mc_security_provider*] - The MCollective security provider
-#  [*mc_security_psk*]    - The MCollective pre shared key
-#  [*main_collective]     - Sets the default collective
-#  [*collectives]         - Sets the collectives a server node belongs to
-#  [*connector]           - The stomp connector to use. Currently only stomp and
-#                           activemq are recognized. Note activemq only supported
-#                           on version 1.3.2+
-#  [*stomp_server]        - Name or ip of stomp server
-#  [*stomp_port]          - Port on stomp server to connect to
-#  [*stomp_user]          - Username used to authenticate on stomp server
-#  [*stomp_passwd]        - Password used to authenticate on stomp server
-#  [*stomp_pool]          - A hash used to supply all parameters needed for advanced
-#                           features like failover pools and ssl
-#  [*classesfile]         - Path to the classes file written by puppet
-#  [*fact_source]         - The type of fact source. Currently only facter and yaml
-#                           are recognized
-#  [*yaml_facter_source]  - List of colon separated yaml files used by yaml fact source
-#  [*plugin_params]       - Hash of parameters passed to mcollective plugins
 #
 # Actions:
 #
 # Requires:
-#
-#   Class['java']
-#   Class['activemq']
 #
 # Sample Usage:
 #
@@ -60,122 +30,35 @@
 #
 # node default {
 #   class { 'mcollective':
-#     version             => 'present',
-#     server              => true,
-#     server_config       => template('mcollective/server.cfg.erb'),
-#     server_config_file  => '/etc/mcollective/server.cfg',
-#     client              => true,
-#     client_config       => template('mcollective/client.cfg.erb'),
-#     client_config_file  => '/home/mcollective/.mcollective',
-#     stomp_server        => 'stomp',
-#   }
-# }
-#
-# To setup stomp failover pools, ssl and plugin parameters:
-# node default {
-#   $stomp_server1 = { host1 => 'stomp1', port1 => '61612', user1 => 'mcollective',
-#     password1 => 'marionette'}
-#   $stomp_server2 = { host2 => 'stomp2', port2 => '6163', user2 => 'mcollective',
-#     password2 => 'marionette', ssl2 => 'true' }
-#
-#   class {
-#     mcollective:
-#       stomp_pool => { pool1 => $stomp_server1, pool2 => $stomp_server2 },
-#       plugin_params => { 'puppetd.puppetd' => '/usr/bin/puppet agent' }
+#     version        => 'present',
+#     enterprise     => false,
+#     server         => true,
+#     client         => false,
+#     manage_plugins => true,
 #   }
 # }
 #
 class mcollective(
-  $version              = $mcollective::params::version,
-  $enterprise           = $mcollective::params::enterprise,
-  $manage_packages      = $mcollective::params::manage_packages,
-  $manage_plugins       = $mcollective::params::manage_plugins,
-  $server               = $mcollective::params::server,
-  $server_config        = $mcollective::params::server_config,
-  $server_config_file   = $mcollective::params::server_config_file,
-  $client               = $mcollective::params::client,
-  $client_config        = $mcollective::params::client_config,
-  $client_config_file   = $mcollective::params::client_config_file,
-  $main_collective      = $mcollective::params::main_collective,
-  $collectives          = $mcollective::params::collectives,
-  $connector            = $mcollective::params::connector,
-  $classesfile          = $mcollective::params::classesfile,
-  $stomp_pool           = $mcollective::params::stomp_pool,
-  $stomp_server         = $mcollective::params::stomp_server,
-  $stomp_port           = $mcollective::params::stomp_port,
-  $stomp_user           = $mcollective::params::stomp_user,
-  $stomp_passwd         = $mcollective::params::stomp_passwd,
-  $mc_security_provider = $mcollective::params::mc_security_provider,
-  $mc_security_psk      = $mcollective::params::mc_security_psk,
-  $fact_source          = $mcollective::params::fact_source,
-  $yaml_facter_source   = $mcollective::params::yaml_facter_source,
-  $plugin_params        = $mcollective::params::plugin_params
+  $version        = $mcollective::params::version,
+  $enterprise     = $mcollective::params::enterprise,
+  $server         = $mcollective::params::server,
+  $client         = $mcollective::params::client,
+  $manage_plugins = $mcollective::params::manage_plugins
 ) inherits mcollective::params
 {
-  $v_bool = [ '^true$', '^false$' ]
-  validate_bool($manage_packages)
   validate_bool($enterprise)
   validate_bool($manage_plugins)
-  validate_re($server_config_file, '^/')
-  validate_re($client_config_file, '^/')
-  validate_re("$server", $v_bool)
-  validate_re("$client", $v_bool)
+  validate_bool("$server", $v_bool)
+  validate_bool("$client", $v_bool)
   validate_re($version, '^[._0-9a-zA-Z:-]+$')
-  validate_re($mc_security_provider, '^[a-zA-Z0-9_]+$')
-  validate_re($mc_security_psk, '^[^ \t]+$')
-  validate_re($fact_source, '^facter$|^yaml$')
-  validate_re($connector, '^stomp$|^activemq$')
-  validate_hash($plugin_params)
 
   $server_real               = $server
   $client_real               = $client
-  $client_config_file_real   = $client_config_file
-  $server_config_file_real   = $server_config_file
-  $stomp_server_real         = $stomp_server
-  $mc_security_provider_real = $mc_security_provider
-  $mc_security_psk_real      = $mc_security_psk
-
-  # Service Name:
-  $service_name = $enterprise ? {
-    true  => 'pe-mcollective',
-    false => 'mcollective',
-  }
 
   if $version == 'UNSET' {
       $version_real = 'present'
   } else {
       $version_real = $version
-  }
-
-  # if no pool hash is provided, create a single pool using defaults
-  if $stomp_pool == 'UNSET' {
-    $stomp_pool_real =
-    {
-      pool1 =>
-      {
-          host1   => $stomp_server,
-          port1   => $stomp_port,
-          user1   => $stomp_user,
-          passwd1 => $stomp_passwd
-      }
-    }
-  }
-  else {
-    validate_hash( $stomp_pool )
-    validate_hash( $stomp_pool['pool1'] )
-    $stomp_pool_real = $stomp_pool
-  }
-  $stomp_pool_size = size(keys($stomp_pool_real))
-
-  if $client_config == 'UNSET' {
-    $client_config_real = template('mcollective/client.cfg.erb')
-  } else {
-    $client_config_real = $client_config
-  }
-  if $server_config == 'UNSET' {
-    $server_config_real = template('mcollective/server.cfg.erb')
-  } else {
-    $server_config_real = $server_config
   }
 
   # Add anchor resources for containment
@@ -185,18 +68,17 @@ class mcollective(
   if $server_real {
     class { 'mcollective::server':
       version         => $version_real,
-      enterprise      => $enterprise,
-      manage_packages => $manage_packages,
-      service_name    => $service_name,
-      config          => $server_config_real,
-      config_file     => $server_config_file_real,
+      manage_packages => $mcollective::params::manage_packages,
+      service_name    => $mcollective::params::service_name,
+      config          => $mcollective::params::server_config_real,
+      config_file     => $mcollective::params::server_config_file_real,
       require         => Anchor['mcollective::begin'],
     }
     # Also manage the plugins
     if $manage_plugins {
       class { 'mcollective::plugins':
         before  => Anchor['mcollective::end'],
-        manage_packages => $manage_packages,
+        manage_packages => $mcollective::params::manage_packages,
       }
     }
   }
@@ -204,13 +86,12 @@ class mcollective(
   if $client_real {
     class { 'mcollective::client':
       version         => $version_real,
-      config          => $client_config_real,
-      config_file     => $client_config_file_real,
-      manage_packages => $manage_packages,
+      config          => $mcollective::params::client_config_real,
+      config_file     => $mcollective::params::client_config_file_real,
+      manage_packages => $mcollective::params::manage_packages,
       require         => Anchor['mcollective::begin'],
       before          => Anchor['mcollective::end'],
     }
   }
-
 }
 
